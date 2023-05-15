74c70161b61ca32bba490f12522fa0b78cb85e2e
fwijaya918 <fwijaya918@gmail.com>
2023-05-15 09:55:25 +0700

    hapus saldo bioskop

---
+++ src/migration/20230515024957-delete-saldo.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * 
+     */
+    await queryInterface.removeColumn('bioskop', 'saldo');
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.addColumn('bioskop', 'saldo', { type: Sequelize.DataTypes.INTEGER });
+  }
+};
